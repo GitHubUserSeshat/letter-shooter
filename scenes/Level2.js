7563c8451b1c2daf94a3957d02d6dc7f585be401
GitHubUserSeshat <51201787+GitHubUserSeshat@users.noreply.github.com>
2026-06-06 12:10:28 +0200

    Refactor letter pool filtering in generateRounds

--- scenes/Level2.js
+++ scenes/Level2.js
@@ -105,8 +105,7 @@ class Level2 extends Phaser.Scene {
 
     generateRounds(count) {
 
-        const letters = this.getLetterPool()
-            .filter(l => l !== "z");
+        const letters = this.getLetterPool().filter(l => l !== "z");
         const rounds = [];
 
         for (let i = 0; i < count; i++) {
